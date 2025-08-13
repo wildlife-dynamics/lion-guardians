223742f07eb4389626b11a41c980f947ccb1f0bc
ttemu <tevintemu@gmail.com>
2025-08-13 14:19:26 +0300

    spec to generate custom collared lions workflow report

--- src/ecoscope-workflows-ext-lion-guardians/ecoscope_workflows_ext_lion_guardians/tasks/__init__.py
+++ src/ecoscope-workflows-ext-lion-guardians/ecoscope_workflows_ext_lion_guardians/tasks/__init__.py
@@ -1,5 +1,32 @@
-from ._example import add_one_thousand
+from ._map_utils import (
+    load_landdx_aoi,
+    download_land_dx,
+    create_map_layers,
+    clean_geodataframe,
+    combine_map_layers,
+    generate_density_grid,
+    build_landdx_style_config,
+    create_view_state_from_gdf,
+    check_shapefile_geometry_type,
+    annotate_gdf_dict_with_geometry_type,
+    create_map_layers_from_annotated_dict,
+    load_map_files,
+    create_layer_from_gdf,
+)
+
 
 __all__ = [
-    "add_one_thousand",
+    "load_landdx_aoi",
+    "download_land_dx",
+    "create_map_layers",
+    "clean_geodataframe",
+    "combine_map_layers",
+    "generate_density_grid",
+    "build_landdx_style_config",
+    "create_view_state_from_gdf",
+    "check_shapefile_geometry_type",
+    "annotate_gdf_dict_with_geometry_type",
+    "create_map_layers_from_annotated_dict",
+    "load_map_files",
+    "create_layer_from_gdf",
 ]
