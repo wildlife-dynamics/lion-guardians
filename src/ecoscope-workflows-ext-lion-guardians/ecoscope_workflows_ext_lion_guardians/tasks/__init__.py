96ac1e6057435fc479f28afb1de6fc465797ed71
ttemu <tevintemu@gmail.com>
2025-08-14 10:33:40 +0300

    add zip function to group multiple grouped layers correctly and test cases for the guardians-workflow

--- src/ecoscope-workflows-ext-lion-guardians/ecoscope_workflows_ext_lion_guardians/tasks/__init__.py
+++ src/ecoscope-workflows-ext-lion-guardians/ecoscope_workflows_ext_lion_guardians/tasks/__init__.py
@@ -1,5 +1,37 @@
+from ._map_utils import (
+    load_landdx_aoi,
+    download_land_dx,
+    create_map_layers,
+    clean_geodataframe,
+    combine_map_layers,
+    generate_density_grid,
+    build_landdx_style_config,
+    create_view_state_from_gdf,
+    check_shapefile_geometry_type,
+    annotate_gdf_dict_with_geometry_type,
+    create_map_layers_from_annotated_dict,
+    load_map_files,
+    create_layer_from_gdf,
+)
+from ._zip import zip_grouped_by_key
 from ._example import add_one_thousand
 
 __all__ = [
+    "view_data",
+    "load_landdx_aoi",
+    "zip_grouped_by_key",
+    "download_land_dx",
+    "create_map_layers",
+    "clean_geodataframe",
+    "combine_map_layers",
+    "generate_density_grid",
+    "build_landdx_style_config",
+    "create_view_state_from_gdf",
+    "check_shapefile_geometry_type",
+    "annotate_gdf_dict_with_geometry_type",
+    "create_map_layers_from_annotated_dict",
+    "load_map_files",
+    "create_layer_from_gdf",
+    "html_to_img",
     "add_one_thousand",
 ]
