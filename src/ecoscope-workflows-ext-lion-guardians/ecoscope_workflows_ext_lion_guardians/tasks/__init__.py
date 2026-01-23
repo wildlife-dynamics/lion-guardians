e6ecffc3b6d40cffba73a6e4a68cdc85f41a3561
Tevin Temu <32607464+ttemu34@users.noreply.github.com>
2026-01-23 18:23:09 +0300

    add word docx template for guardians report (#6)
    * add word docx template for guardians report
    
    * add test-cases.yaml

--- src/ecoscope-workflows-ext-lion-guardians/ecoscope_workflows_ext_lion_guardians/tasks/__init__.py
+++ src/ecoscope-workflows-ext-lion-guardians/ecoscope_workflows_ext_lion_guardians/tasks/__init__.py
@@ -3,6 +3,29 @@ from ._collared_lions_context import (
     create_context_page_lg,
     merge_cl_files,
     create_collared_lions_grouper_ctx,
+    create_guardians_ctx_cover,
+    create_guardians_grouper_ctx,
 )
 
-__all__ = ["create_cl_ctx_cover", "create_context_page_lg", "merge_cl_files", "create_collared_lions_grouper_ctx"]
+from ._tabular import extract_date_parts
+
+from ._retrieve_patrols import (
+    get_patrol_observations_from_patrols_dataframe,
+    get_patrols_from_combined_parameters,
+    get_patrol_observations_from_patrols_dataframe_and_combined_params,
+    get_event_type_display_names_from_events_aliased,
+)
+
+__all__ = [
+    "create_cl_ctx_cover",
+    "create_context_page_lg",
+    "merge_cl_files",
+    "create_collared_lions_grouper_ctx",
+    "create_guardians_ctx_cover",
+    "extract_date_parts",
+    "create_guardians_grouper_ctx",
+    "get_patrol_observations_from_patrols_dataframe",
+    "get_patrols_from_combined_parameters",
+    "get_patrol_observations_from_patrols_dataframe_and_combined_params",
+    "get_event_type_display_names_from_events_aliased",
+]
